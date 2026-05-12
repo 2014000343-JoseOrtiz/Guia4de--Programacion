8114b8aec67ba6a51398997e7175a483dda137ea
2014000343-JoseOrtiz <j.ortiz.5cdb@gmail.com>
2026-05-11 18:41:31 -0600

    Add files via upload

---
+++ Act1_Lecc11_inciso1.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int capacidadTotal = 200;
+        int autosIngresados = 0;
+        int cajonesLibres;
+
+        do
+        {
+            cajonesLibres = capacidadTotal - autosIngresados;
+
+            if (cajonesLibres > 0)
+            {
+                Console.WriteLine("Cajones disponibles: cajonesLibres");
+                Console.WriteLine("Presiona Enter para registrar la entrada");
+                Console.ReadLine();
+                autosIngresados++;
+            }
+
+        } while (autosIngresados < capacidadTotal);
+
+        Console.WriteLine("Ya no hay cajones libres. Estacionamiento lleno.");
+    }
+}
